394afa6df6bbfdd3f96f4c3625e8bbba9618f178
bruce5283 <bcarrington7@gmail.com>
2022-07-08 12:26:15 -0400

    Updated

--- function.js
+++ function.js
@@ -21,7 +21,7 @@ window.function = function (time, fweight, align, fsize, width, height) {
    <!-- Display the countdown timer in an element -->
 <div class = "container">
 <p id="pre"></p>
-<button id="btn" disabled onclick = "playBuffer()">Start Timer</button><br>
+<button id="btn" onclick = "playBuffer()">Start Timer</button><br>
 </div>
 <audio controls id = "audio" src = "https://www.fesliyanstudios.com/play-mp3/4385" />
 <style>
